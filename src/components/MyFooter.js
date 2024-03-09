ee52ec483b8f70b15c4ce5d77eff693aef5d2660
hxerimione <purejang98@gmail.com>
2024-03-09 21:46:33 +0900

    info setting

---
+++ src/components/MyFooter.js
@@ -0,0 +1,4 @@
+const MyFooter = ({}) => {
+    return <footer>footer</footer>;
+};
+export default MyFooter;
--- src/pages/MyInfo.js
+++ src/pages/MyInfo.js
@@ -1,7 +1,26 @@
+import MyFooter from '../components/MyFooter';
+import MyHeader from '../components/MyHeader';
+
 const MyInfo = ({}) => {
+    const env = process.env;
+    env.PUBLIC_URL = env.PUBLIC_URL || '';
     return (
         <div className="myinfo">
-            <p>MyInfo</p>
+            <MyHeader searchKeyword={'검색어를 입력하세요'} />
+            <div className="myinfo_wrapper">
+                <section className="myinfo_section">
+                    <img
+                        src={process.env.PUBLIC_URL + `assets/myimage.jpg`}
+                        width="150px"
+                    />
+                    <h4>장혜림</h4>
+                </section>
+                <section className="mygraph_section">내 별점 그래프</section>
+                <section className="mylist_section">
+                    내 평가 목록 최신순
+                </section>
+            </div>
+            <MyFooter />
         </div>
     );
 };
