5a252c9496277c2b2ab27a198fdada46008ee76d
hxerimione <purejang98@gmail.com>
2024-03-24 22:06:05 +0900

    라우팅 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
-import New from './pages/New';
 import MyInfo from './pages/MyInfo';
-import Edit from './pages/Edit';
-import Review from './pages/Review';
 import {
     createContext,
     useContext,
@@ -124,20 +121,8 @@ function App() {
                                 element={<Home />}
                             />
                             <Route
-                                path="/new"
-                                element={<New />}
-                            />
-                            <Route
                                 path="/myInfo"
                                 element={<MyInfo />}
-                            />
-                            <Route
-                                path="/edit/:id"
-                                element={<Edit />}
-                            />
-                            <Route
-                                path="/review/:id"
-                                element={<Review />}
                             />
                             <Route
                                 path="/result"
